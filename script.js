dd01bfc9bfe65e153cfd4ec29000e367097b5703
yogeshsapare <117711304+yogeshsapare@users.noreply.github.com>
2023-04-05 22:57:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+
+function getMessage() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("Hello, world!");
+    }, 1000);
+  });
+}
+
+getMessage().then((message) => {
+  document.getElementById("output").textContent = message;
+});
